64fb4d057e423ce222e086c9a9b530f16953ef85
harneetchug <harneetchugh.cec@gmail.com>
2026-05-10 21:47:07 +0200

    updated with KeyVault config

--- src/main/java/com/example/accountstatementservice/KeyVaultConfig.java
+++ src/main/java/com/example/accountstatementservice/KeyVaultConfig.java
@@ -3,8 +3,11 @@ package com.example.accountstatementservice;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class KeyVaultConfig {
@@ -15,5 +18,18 @@ public class KeyVaultConfig {
                 .vaultUrl("https://banking-keyvault.vault.azure.net/")
                 .credential(new DefaultAzureCredentialBuilder().build())
                 .buildClient();
+    }
+
+    @Bean
+    public DataSource dataSource(SecretClient secretClient) {
+
+        String password = secretClient.getSecret("sql-password").getValue();
+
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:sqlserver://bankingsqlserver.database.windows.net:1433;database=accountstatementdb");
+        ds.setUsername("sqladmin");
+        ds.setPassword(password);
+
+        return ds;
     }
 }
